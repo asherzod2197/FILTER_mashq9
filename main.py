1283823df80b5f17d581d490a40e9ce76ab22ef5
Ataxanov <asherzod2197@gmail.com>
2026-04-13 12:53:26 +0500

    Add multiple filter examples using lambda functions

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+# 41-45
+roy = [3, 6, 9]
+print(list(filter(lambda x: x != 6, roy)))
 
+roy = ["aa", "bbb", "cccc"]
+print(list(filter(lambda x: len(x) >= 3, roy)))
+
+roy = [50, 60, 70]
+print(list(filter(lambda x: x > 55, roy)))
+
+roy = ["python", "js"]
+print(list(filter(lambda x: len(x) > 2, roy)))
+
+roy = [2, 3, 4, 5]
+print(list(filter(lambda x: x % 2 == 0, roy)))
